47058b2db9bd85a3149a9b2a970b98ac34e3bc2f
Praveen Premaratne <praveen.premaratne@groupcdg.com>
2017-10-23 23:39:30 +0100

    Topic service variables changed to Subjects, Topic Detail component updated
    - SelectedTopic and topics chage to type of Subject to allow keep last value on memory
    - Set method updated to be compatible with Subject
    - New Get method implemented
    - Topic Detail component code rearranged and new service subscription added to fetch last given value
    
    ** NPM PACKAGE UPDATE MAY REQUIRE **

--- client/src/app/service/topic.service.ts
+++ client/src/app/service/topic.service.ts
@@ -2,28 +2,58 @@ import {Injectable} from "@angular/core";
 import {Topic} from "../model/topic";
 import {Headers, Http, RequestOptions} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
+import {Subject} from "rxjs/Subject";
+import {Observable} from "rxjs/Observable";
+
 
 @Injectable()
 export class TopicService {
 
-  selectedTopic: Topic;
-  topics: Topic[];
+  constructor(private http: Http) {
+  }
+  
+  // Two variable are now will keep last value given until the service not destroyed.
+  selectedTopic: Subject<Topic> = new Subject();
+
+
+  topics: Subject<Topic[]> = new Subject();
 
   // selected selectedTopic to share data between Topic and TopicDetail component
   setSelectedTopic(topic: Topic) {
-    this.selectedTopic = topic;
+    // This will set new value to the variable
+    this.selectedTopic.next(topic);
+
   }
 
   setTopics(topics: Topic[]) {
     console.log("setting topics " + topics);
-    this.topics = topics;
+    // This will set new value to the variable
+    this.topics.next(topics);
+
   }
 
+  /**
+   * This method will allow component to subscribe to the selectedTopic as if it was a service
+   * @returns {Observable<Topic>}
+   */
+  getSelectedTopic(): Observable<Topic> {
+    return this.selectedTopic.asObservable();
+  }
+
+
+  /**
+   * This method will allow compinent to subscribe to the #topics array
+   * @returns {Observable<Topic[]>}
+   */
+  getLocalTopics(): Observable<Topic[]> {
+    return this.topics.asObservable();
+  }
   private headers = new Headers({'Content-Type': 'application/json'});
+
   result: Promise<Topic[]>;
-  constructor(private http: Http) { }
+
   getTopics(): Promise<Topic[]> {
-    this.result =  this.http.get("/api/topics")
+    this.result = this.http.get("/api/topics")
       .toPromise()
       .then(response => response.json() as Topic[])
       .catch(this.handleError);
@@ -37,8 +67,8 @@ export class TopicService {
   }
 
 
-  putTopic(topic: Topic) : void {
-    let options = new RequestOptions({ headers: this.headers });
+  putTopic(topic: Topic): void {
+    let options = new RequestOptions({headers: this.headers});
     let endpoint = "/api/topics/" + topic.id;
     this.http.put(endpoint, JSON.stringify(topic), options)
       .toPromise()
@@ -47,7 +77,7 @@ export class TopicService {
   }
 
   postTopic(topic: Topic): void {
-    let options = new RequestOptions({ headers: this.headers });
+    let options = new RequestOptions({headers: this.headers});
     let endpoint = "/api/topics/";
     this.http.post(endpoint, JSON.stringify(topic), options)
       .toPromise()
@@ -56,7 +86,7 @@ export class TopicService {
   }
 
   deleteTopic(topic: Topic): void {
-    let options = new RequestOptions({ headers: this.headers });
+    let options = new RequestOptions({headers: this.headers});
     let endpoint = "/api/topics/" + topic.id;
     this.http.delete(endpoint, options)
       .toPromise()
--- client/src/app/topic-detail/topic-detail.component.ts
+++ client/src/app/topic-detail/topic-detail.component.ts
@@ -8,14 +8,26 @@ import {TopicService} from "../service/topic.service";
   styleUrls: ['./topic-detail.component.css']
 })
 export class TopicDetailComponent implements OnInit {
-  ngOnInit(): void {
-    console.log(this.topicService.selectedTopic);
-  }
 
   selectedTopic: Topic;
   topics: Topic[];
 
   constructor(private topicService: TopicService){}
+
+  ngOnInit(): void {
+    //Subscribe to the service to get newest updates. Should update asynchronously
+    this.topicService.getSelectedTopic().subscribe((topic: Topic) => {
+      console.log(topic);
+      this.selectedTopic = topic;
+    });
+
+    // Subscribe to topics to get newest changes from service. Should update asynchronously
+    this.topicService.getLocalTopics().subscribe((topics: Topic[]) => {
+      this.topics = topics;
+      console.log(topics);
+    });
+    // console.log(this.topicService.selectedTopic);
+  }
 
   saveTopicName(topic: Topic): void {
     console.log(topic);
